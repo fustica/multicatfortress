3a5c5d61ca291d4dc68e134f25678e6db7aa9531
EROR <ilja.erik.krylov@gmail.com>
2024-11-02 19:30:49 +0000

    update

--- core/src/main/kotlin/fustica/multicatfortress/MewsScreen.kt
+++ core/src/main/kotlin/fustica/multicatfortress/MewsScreen.kt
@@ -3,16 +3,42 @@ package fustica.multicatfortress
 import com.badlogic.gdx.Gdx
 import com.badlogic.gdx.InputProcessor
 import com.badlogic.gdx.Screen
+import com.badlogic.gdx.graphics.Color
 import com.badlogic.gdx.graphics.GL20
+import com.badlogic.gdx.graphics.g2d.BitmapFont
+import com.badlogic.gdx.graphics.g2d.SpriteBatch
+import com.badlogic.gdx.math.Vector2
 
 class MewsScreen: Screen, InputProcessor{
-    override fun show() {
 
+    var mews = 0
+
+    //text
+    lateinit var batch: SpriteBatch
+    lateinit var font: BitmapFont
+
+    //clics
+    //var mouseClickCoordinate = Vector2(0f,0f)
+    var mouseClicked = false
+
+
+
+
+    override fun show() {
+        //text
+        batch = SpriteBatch ()
+        font = BitmapFont()
+        font.setColor(Color.BLACK)
     }
 
     override fun render(p0: Float) {
-        Gdx.gl.glClearColor(1f, 0f, 0f, 1f)
+        Gdx.gl.glClearColor(0.7f, 0.7f, 0.7f, 1f)
         Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT)
+
+        //text
+        batch.begin()
+        font.draw(batch, "Mews: "+mews, 100f, 400f)
+        batch.end ()
     }
 
     override fun resize(p0: Int, p1: Int) {
@@ -40,6 +66,7 @@ class MewsScreen: Screen, InputProcessor{
     }
 
     override fun keyUp(p0: Int): Boolean {
+        mouseClicked = false
         return true
     }
 
@@ -48,6 +75,10 @@ class MewsScreen: Screen, InputProcessor{
     }
 
     override fun touchDown(p0: Int, p1: Int, p2: Int, p3: Int): Boolean {
+        //mouseClickCoordinate.x = p0.toFloat()
+        //mouseClickCoordinate.y = p1.toFloat()
+        mouseClicked = true
+        mews += 1
         return true
     }
 
